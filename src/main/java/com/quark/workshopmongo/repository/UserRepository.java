28eaa5abc216a4ea68788db4e396e5bb77e5bc2c
francisco <frmauro8@hotmail.com>
2020-02-14 07:18:38 -0300

    conectando ao mongodb com repository e service

---
+++ src/main/java/com/quark/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.quark.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.quark.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
