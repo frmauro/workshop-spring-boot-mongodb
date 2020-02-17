7c5566c3aba2bc82154b01071d30ec62a669f28a
francisco <frmauro8@hotmail.com>
2020-02-17 20:27:41 -0300

    Tratando execeção customizada

--- src/main/java/com/quark/workshopmongo/services/UserService.java
+++ src/main/java/com/quark/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.quark.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,17 @@ import com.quark.workshopmongo.repository.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository rep;
 
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return rep.findAll();
 	}
+
+	public User findById(String id) {
+		Optional<User> obj = rep.findById(id);
+		return obj.orElseThrow();
+	}
+
 }
