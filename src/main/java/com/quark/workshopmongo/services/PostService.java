85d0db0f6320ae3e497f75d97350728fdf0d01ca
francisco <frmauro8@hotmail.com>
2020-03-23 20:39:22 -0300

    add post resource and service

---
+++ src/main/java/com/quark/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.quark.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quark.workshopmongo.domain.Post;
+import com.quark.workshopmongo.repository.PostRepository;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository rep;
+
+	public Post findById(String id) {
+		Optional<Post> obj = rep.findById(id);
+		return obj.orElseThrow();
+	}
+
+}
